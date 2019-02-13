0a4f1a61bdc6aa43f2527f2fdb3c680ec58d0125
Mark Sperle-Weiler <40145728+mdsw81@users.noreply.github.com>
2019-02-13 13:49:18 -0600

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+    <footer>
+        <h2>&copy; Mark SperleWeiler 2019</h2>
+    </footer>
+)
+
+export default Footer;
+
